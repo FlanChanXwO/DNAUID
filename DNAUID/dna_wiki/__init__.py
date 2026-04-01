7952017b27f1a731fe1a836fa9770806df19b97a
tyql688 <yury.zero.21@gmail.com>
2026-04-01 16:18:10 +0800

    🆕 武器，魔灵图鉴

--- DNAUID/dna_wiki/__init__.py
+++ DNAUID/dna_wiki/__init__.py
@@ -2,13 +2,13 @@ from gsuid_core.sv import SV
 from gsuid_core.bot import Bot
 from gsuid_core.models import Event
 
-from .wiki import get_role_wiki
+from .wiki import get_wiki
 from ..utils.constants.constants import PATTERN
 
 sv_dna_wiki = SV("dna图鉴")
 
 
-@sv_dna_wiki.on_regex(rf"^(?P<char_name>{PATTERN})(?:图鉴|wiki|Wiki|WIKI)$", block=True)
-async def send_role_wiki(bot: Bot, ev: Event):
-    char_name = ev.regex_dict.get("char_name", "")
-    await get_role_wiki(bot, ev, char_name)
+@sv_dna_wiki.on_regex(rf"^(?P<name>{PATTERN})(?:图鉴|wiki|Wiki|WIKI)$", block=True)
+async def send_wiki(bot: Bot, ev: Event):
+    name = ev.regex_dict.get("name", "")
+    await get_wiki(bot, ev, name)
--- DNAUID/dna_wiki/wiki.py
+++ DNAUID/dna_wiki/wiki.py
@@ -1,25 +1,42 @@
 from pathlib import Path
 
 from gsuid_core.bot import Bot
+from gsuid_core.logger import logger
 from gsuid_core.models import Event
 from gsuid_core.utils.image.convert import convert_img
 
 from ..utils.msgs.notify import dna_not_found
-from ..utils.name_convert import alias_to_char_name
+from ..utils.name_convert import alias_to_char_name, alias_to_weapon_name
 
-WIKI_PATH = Path(__file__).parent / "texture2d" / "role"
+WIKI_PATH = Path(__file__).parent / "texture2d"
+ROLE_PATH = WIKI_PATH / "role"
+WEAPON_PATH = WIKI_PATH / "weapon"
+SPIRIT_PATH = WIKI_PATH / "spirit"
 
 
-async def get_role_wiki(bot: Bot, ev: Event, char_name: str):
-    real_name = alias_to_char_name(char_name)
-    if not real_name:
-        await dna_not_found(bot, ev, f"角色别名【{char_name}】")
+async def get_wiki(bot: Bot, ev: Event, name: str):
+    # 角色
+    real_name = alias_to_char_name(name)
+    if real_name:
+        img_path = ROLE_PATH / f"{real_name}.webp"
+        if img_path.exists():
+            logger.info(f"[二重螺旋] 发送{real_name}角色图鉴")
+            await bot.send(await convert_img(img_path))
+            return
+
+    # 武器
+    weapon_name = alias_to_weapon_name(name)
+    img_path = WEAPON_PATH / f"{weapon_name}.webp"
+    if img_path.exists():
+        logger.info(f"[二重螺旋] 发送{weapon_name}武器图鉴")
+        await bot.send(await convert_img(img_path))
         return
 
-    img_path = WIKI_PATH / f"{real_name}.webp"
-    if not img_path.exists():
-        await dna_not_found(bot, ev, f"角色【{real_name}】图鉴")
+    # 魔灵
+    img_path = SPIRIT_PATH / f"{name}.webp"
+    if img_path.exists():
+        logger.info(f"[二重螺旋] 发送{name}魔灵图鉴")
+        await bot.send(await convert_img(img_path))
         return
 
-    img = await convert_img(img_path)
-    await bot.send(img)
+    await dna_not_found(bot, ev, f"【{name}】图鉴")
